78e7cd1266f2af37d7c3f7d2ccae3bbbaacfcecc
abhinay-tiwari01 <tabhinay3@gmail.com>
2025-09-08 03:33:59 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Recipe_Finder/',  // <-- This tells Vite where to look for assets
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Recipe-Finder/' : '/'
 });
